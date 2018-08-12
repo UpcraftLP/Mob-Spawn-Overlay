60987bb9228c1f1287b1c856c2d81c9ef2d5771d
UpcraftLP <j-schirra@gmx.de>
2018-08-12 23:02:08 +0200

    basic mod files

---
+++ src/main/java/com/github/upcraftlp/spawnoverlay/MobSpawnOverlay.java
@@ -0,0 +1,11 @@
+package com.github.upcraftlp.spawnoverlay;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SuppressWarnings("unused")
+public class MobSpawnOverlay {
+
+    public static final Logger log = LogManager.getLogger("Mob-Spawn-Overlay");
+
+}
